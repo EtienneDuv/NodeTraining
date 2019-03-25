8555bf183570f1bfe615d7d85ce6125d3501aa9e
Adalon <etienne.duverney@viacesi.fr>
2019-03-25 15:12:52 +0100

    Filesystem

--- FirstApp/app.js
+++ FirstApp/app.js
@@ -1,25 +1,7 @@
-const   events = require('events'),
-        util = require('util');
+const fs = require('fs');
 
-
-var Person = function(name){
-    this.name = name;
-};
-
-util.inherits(Person, events.EventEmitter);
-
-
-var etienne = new Person('Etienne'),
-    marie = new Person('Marie'),
-    clemi = new Person('Clémi');
-
-let people = [etienne, marie, clemi];
-
-
-people.forEach(person => {
-    person.on('speak', function (msg) {
-       console.log(`${person.name} said: ${msg}`); 
-    });
+fs.readFile('README.txt', 'utf8', function(err, data) {
+    fs.writeFileSync('WRITEME.txt', data)
 });
 
-etienne.emit('speak', 'Wesh les kheys !');
+console.log('This happened !');
