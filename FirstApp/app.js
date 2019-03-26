6975d594a13c6878d90a77dcbafcda7ea157ede8
Adalon <etienne.duverney@viacesi.fr>
2019-03-26 13:37:50 +0100

    Create + Remove dir

--- FirstApp/app.js
+++ FirstApp/app.js
@@ -1,15 +1,27 @@
 const fs = require('fs');
+const path = require('path');
 
-fs.readFile('README.txt', 'utf8', function(err, data) {
-    fs.writeFileSync('WRITEME.txt', data)
-});
-
-console.log('This happened !');
+if (fs.existsSync('Example')) {
+    fs.readdirSync('Example').forEach(file => {
+        fs.unlinkSync(path.join(__dirname, 'Example\\'+file), err => {
+            console.log(file);
+            if (err) throw err;
+        });
+        fs.rmdirSync('Example');
+        console.log('Example folder deleted with its content')
+    });
+};
 
 if (!fs.existsSync('Example')) {
     fs.mkdirSync('Example');
+    console.log(`Example folder created`);
 };
 
-if (fs.existsSync('Example')) {
-    fs.rmdirSync('Example');
-};
+fs.readFile('readme.txt', 'utf8', function (err, data) {
+    fs.writeFile('./Example/writeme.txt', data, function(err) {
+        if (err) console.log(err);
+        console.log(`Text written`);
+    });
+});
+
+
