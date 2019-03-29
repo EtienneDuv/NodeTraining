1d05bced794a23593ef9dddeefac6fdeb9ecb78b
Adalon <etienne.duverney@viacesi.fr>
2019-03-29 15:58:28 +0100

    Filesystem Streams

--- FirstApp/app.js
+++ FirstApp/app.js
@@ -1,10 +1,10 @@
 var http = require('http');
+var fs = require('fs');
 
-var server = http.createServer(function(req, res) {
-    console.log('Request was made: ' + req.url);
-    res.writeHead('200', {'Content-Type': 'text/plain'});
-    res.end('Displayed text');
+var myReadStream = fs.createReadStream(__dirname + '/README.txt', 'utf8');
+var myWriteStream = fs.WriteStream(__dirname + '/WRITEME.txt', 'utf8');
+
+myReadStream.on('data', function (chunk) {
+    console.log(chunk);
+    myWriteStream.write(chunk);
 });
-
-server.listen(3000, '127.0.0.1');
-console.log('Code reached here');
