efce5d91fc599e8ea5ffa552b2e1fd074ce06cc1
Adalon <etienne.duverney@viacesi.fr>
2019-03-29 15:22:47 +0100

    Basic server

--- FirstApp/app.js
+++ FirstApp/app.js
@@ -1,27 +1,10 @@
-const fs = require('fs');
-const path = require('path');
+var http = require('http');
 
-if (fs.existsSync('Example')) {
-    fs.readdirSync('Example').forEach(file => {
-        fs.unlinkSync(path.join(__dirname, 'Example\\'+file), err => {
-            console.log(file);
-            if (err) throw err;
-        });
-        fs.rmdirSync('Example');
-        console.log('Example folder deleted with its content')
-    });
-};
-
-if (!fs.existsSync('Example')) {
-    fs.mkdirSync('Example');
-    console.log(`Example folder created`);
-};
-
-fs.readFile('readme.txt', 'utf8', function (err, data) {
-    fs.writeFile('./Example/writeme.txt', data, function(err) {
-        if (err) console.log(err);
-        console.log(`Text written`);
-    });
+var server = http.createServer(function(req, res) {
+    console.log('Request was made: ' + req.url);
+    res.writeHead('200', {'Content-Type': 'text/plain'});
+    res.end('Displayed text');
 });
 
-
+server.listen(3000, '127.0.0.1');
+console.log('Code reached here');
