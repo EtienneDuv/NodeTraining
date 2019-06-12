e077ab65637bf3fa8f62a9fd2654744828799523
Adalon <etienne.duverney@viacesi.fr>
2019-06-12 11:32:40 +0200

    Mongo connexion

--- ToDoList/controllers/todoController.js
+++ ToDoList/controllers/todoController.js
@@ -1,29 +1,45 @@
-var data = [{ item: 'bread' }, { item: 'Mawie' }, { item: 'Hello there !' }, ];
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
-var bodyParser = require("body-parser");
-var ajax = require("../public/ajax");
+// MongoDB Management
+// Connect
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb+srv://Adalon:Dagoba74@cluster0-07q8b.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true })
+        .then(res => console.log("Connected to DB"))
+        .catch(err => console.log(err));
+// Create schema
+const todoSchema = new mongoose.Schema({
+    item: String
+})
+const Todo = mongoose.model("Todo", todoSchema);
 
-var urlEncodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app){
-    app.get('/todo', (req, res) => {
-        console.log("GET");
+    app.get("/todo", (req, res) => {
         res.status(200);
-        res.setHeader('Content-type', 'text/html');
-        res.render('todo', { list: data, title: 'Todo List' });
+        res.setHeader("Content-type", "text/html");
+        //GET data from mongo
+        Todo.find({}, function(err, data){
+            if (err) throw err;
+            res.render("todo", { title: "Todo List", list: data });
+        })
     });
 
-    app.post('/todo', urlEncodedParser, (req, res) => {
-        console.log("POST");
-        data.push(req.body);
-        const form = {url: req.path, data: req.body};
-        const renderTodo = () => { res.render('todo', { list: data, title: 'Todo List' }) };
-        ajax.makeAjaxRequest(form.url, 'POST', JSON.stringify(form.data),  renderTodo);
+    app.post("/todo", urlEncodedParser, (req, res) => {
+        //GET data from view (post request) and save it in Mongo
+        const newTodo = Todo(req.body).save(function(err, data){
+            if (err) throw err;
+            res.json(data);
+        });
     });
 
-    app.delete('/todo', function (req, res) {
-        console.log("DELETE");
-        
-    });    
+    app.delete("/todo/:item", function (req, res) {
+        itemToDelete = req.params.item;
+        //delete the item from Mongo
+        Todo.find({item: req.params.item.replace(/-/g, " ")}).deleteOne(function(err, data){
+            if (err) throw err;
+            res.json(data);
+        });
+    });
 }
-
